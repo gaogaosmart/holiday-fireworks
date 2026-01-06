7c4244251bd3ec40fdf48a5f707c5d671468d34a
gaogaosmart <167855241+gaogaosmart@users.noreply.github.com>
2026-01-06 15:57:47 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+window.configData = {
+  countdownTargetTime: "2026-01-23T00:00:00",
+  autoLaunch: true,
+  longExposure: true,
+  wordShell: true,
+  fireworkText: ["放假啦", "Happy Holiday", "🎆🎉"]
+};
